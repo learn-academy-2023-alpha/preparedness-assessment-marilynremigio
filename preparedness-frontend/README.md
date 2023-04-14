2533ea12b80bb8da9dc7c9f67f817ebc0bae30c5
Marilyn Remigio <marilynremigio@gmail.com>
2023-04-13 20:28:13 -0600

    completes preparedness assessment frontend with notes to reference on elements

--- preparedness-frontend/README.md
+++ preparedness-frontend/README.md
@@ -1,1 +1,45 @@
 # Preparedness Assessment
+
+## Frontend Assessment
+
+### Frontend Styling Acceptance Criteria
+[x] The header text color is white and is 50px
+[x] The header is centered on the page
+[x] The input label text color is white and is 35px
+[x] The input label is centered on the page
+[x] The input field text is 18px
+[x] The input field should take up 50% of the width of the page
+[x] The input field is centered on the page
+[x] There is 12px of padding on the left and the right of the input field
+[x] There is 6px of padding on the top and the bottom of the input field
+[x] The background is this image
+[x] The buttons are centered under the input field
+[x] The background color of the buttons are #0dcaf0
+[x] The color of the text on the buttons is white
+
+[x] There is 10 px of padding on all sides of the button
+[x] There is 20px between the two buttons
+[x] There is 20px between the bottom of the input and the top of the buttons
+
+
+
+## Backend Assessment
+
+### Prompt: What is the relationship between a model called person, a model called house, and a model called car? (Hint: there is more than one right answer!)
+
+#### House as Primary Key
+A House has_many persons, a Person belongs_to a House
+A House has_many cars, a Car belongs_to a House
+
+#### Person as Primary Key
+A Person has_many cars, a car belongs_to a Person
+A Person has_many credit_cards, a credit_card belongs_to a Person
+
+#### Car as Primary Key
+A Car has_many persons, a person belongs_to a Car
+A Car has_many owners, an owner belongs_to a Car
+
+#### Owner as Primary Key
+An Owner has_many houses, a house belongs_to an Owner
+An Owner has_many cars, a car belongs_to an Owner
+An Owner has_many credit_cards, a credit_card belongs_to an Owner
--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,21 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+
+  const [modal, setModal] = useState(false)
+  const [inputName, setInputName] = useState("")
+
+  const toggle = () => {
+    if(inputName === '')
+      alert("Please enter your name.")
+    else
+      setModal(!modal)
+  }
+
+  const handleChange = (e) => {
+    setInputName(e.target.value)
+  // The e is the argument of an event handler, e.target.value is the value property of the text entered in the input field
+  }
+
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
+      <br />
+      <br />
       <div className="form">
-        <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+        <div className="input-field">
+          <Input type="text" value={inputName} onChange={handleChange} />
+          {/* The onChange event in React detects when the value of an input element changes. Add an onChange Handler to an Input. Pass an Input Value to a Function in a React Component. Storing an Input Value Inside of State. */}
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
-      </div>
+          <Button 
+            className="click-me"
+            type='submit' 
+            style={{background: '#0dcaf0', padding: '10px'}} 
+            onClick={toggle} 
+            // onClick triggers toggle function which checks for user inputted name, and sets modal = true, which opens the modal
+          >
+          Click Me
+          </Button>
+          <Button 
+            className="reset"
+            style={{background: '#0dcaf0', padding: '10px'}} 
+            onClick={handleChange} 
+            // onClick triggers handleChange which looks for an event (user typing), if nothing was typed and user clicks reset button, then e.target.value has no value and the text field clears to empty
+          >
+            Reset
+          </Button>
+        </div>
+        <ModalComponent modal={modal} toggle={toggle} inputName={inputName}/>
     </div>
   )
 }
